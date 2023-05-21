10753975d0e52fa053c8a062546d050b35a7efaf
gregdom <gregory_dom@hotmail.com>
2023-05-21 14:46:40 -0300

    21-05-2023 14:46

---
+++ src/shared/http/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Obter o diretório atual do arquivo
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Configurar o middleware para servir os arquivos estáticos do Vue
+// Altere a linha que configura o middleware para servir os arquivos estáticos
+app.use(express.static(join(__dirname, '../../../', 'dist')));
+
+// Altere a linha que configura a rota para servir a página inicial do Vue
+app.get('/', (req, res) => {
+  res.sendFile(join(__dirname, '../../../', 'dist', 'index.html'));
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando http://localhost:${port}`);
+});
