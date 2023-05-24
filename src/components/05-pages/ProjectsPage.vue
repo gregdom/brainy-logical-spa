8b92fc808b32afb31bb8da357952ecf61f105346
gregdom <gregory_dom@hotmail.com>
2023-05-24 12:28:07 -0300

    24-05-2023 12:28

--- src/components/05-pages/ProjectsPage.vue
+++ src/components/05-pages/ProjectsPage.vue
@@ -6,9 +6,16 @@
 
 <script>
 import { ProjectsTemplate } from '../04-templates'
+import { mapActions } from 'vuex'
 
 export default {
   components: { ProjectsTemplate },
+  methods: {
+    ...mapActions(['populateStoreArr']),
+  },
+  created() {
+    this.populateStoreArr()
+  },
 }
 </script>
 
